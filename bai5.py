83a6f397de27c43d26262adc3c89b65eb1712aea
haidang <nguyenhidang15092012@gmail.com>
2026-08-11 20:29:51 +0700

    Ronaldo better than messi

---
+++ bai5.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+#st.write("123")
+#st.write("hello")
+#stop:can trôn xê
+#python -m stremlit run tènile.py
+# python -m streamlit hello
+
+#st.title("happy sunday")
+#name = st.text_input("what's your name")
+#st.write("hi",name)
+
+st.title("giới thiệu bản thân")
+st.write("tôi tên là: lê tấn phát")
+st.write("ngày tháng năm sinh:1/11/2004")
+st.write("môn học yêu thích: hoá và tiếng anh")
+st.write("sở thích: thích xem tv ")
